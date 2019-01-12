878e2e3d9cdc79301330cd4418fdf6473fa4dc00
tetue <tetue@rezo.net>
2019-01-12 18:06:19 +0100

    Compatibilité étendue à SPIP 3.2.* (non testé)

---
+++ lang/paquet-fairpub_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/court-circuit/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'fairpub_description' => 'Inserer une publicit&eacute; qui respecte l\'utilisateurice&nbsp;:
+	- uniquement si il vient d\'un moteur de recherche,
+	- s\'il n\'est pas identifi&eacute;.
+	Un lien « Pas de pub s\'il vous pla&icirc;t ! » permet de desactiver la publicit&eacute; pour ne plus la voir.',
+	'fairpub_nom' => 'NoPub',
+	'fairpub_slogan' => 'Pas de pub s\'il vous plaît !'
+);
